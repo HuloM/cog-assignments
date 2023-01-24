c9b465838114c379fe31f03c408aecaecf8af105
matt <matthieu.l.dubray@gmail.com>
2023-01-24 07:36:15 -0800

    day07 assignment q3

---
+++ src/com/cogent/assignments/day07/q3/TestTreeSet.java
@@ -0,0 +1,37 @@
+package com.cogent.assignments.day07.q3;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TestTreeSet {
+
+    public static void main(String[] args) {
+        TreeSet<String> product = new TreeSet<>();
+
+        product.add("table");
+        product.add("chair");
+        product.add("computer");
+        product.add("table");
+        product.add("computer");
+        product.add("mouse");
+        product.add("keyboard");
+
+        Iterator<String> itr = product.iterator();
+
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
+        System.out.println("first: " + product.first() +
+                "\nlast: " + product.last());
+
+        System.out.println("size: " + product.size());
+
+        product.remove("keyboard");
+        System.out.println(product);
+        System.out.println("size: " + product.size());
+
+
+    }
+}
